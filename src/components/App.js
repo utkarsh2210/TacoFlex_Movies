c3de5a73a527c94ab20034274ff778a4e432ef05
unknown <utkarshmaheria@gmail.com>
2020-09-30 00:46:28 +0530

    changed the state

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-
+import { addMovies } from '../actions';
 class App extends React.Component {
   
   componentDidMount () {
@@ -13,16 +13,13 @@ class App extends React.Component {
     });
     // make api call
     // dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
 
     console.log(this.props.store.getState());
   }
 
   render() {
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState(); // {list: [], favourites: []}
     return (
       <div className="App">
       <Navbar />
@@ -33,7 +30,7 @@ class App extends React.Component {
         </div>
 
         <div className="list">
-          {movies.map((movie, index) => (
+          {list.map((movie, index) => (
             <MovieCard movie={movie} key={`movies-${index}`}/>
           ))}
         </div>
